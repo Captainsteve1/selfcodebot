6dc64a969d7af97cae1ffa8a4023837219466f0e
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2022-12-15 22:26:20 +0530

    Update tbot.py

--- tbot.py
+++ tbot.py
@@ -4,6 +4,13 @@ Token = "5809892449:AAFE_GhVSKIcTbdya4WIg0qs9AXk5cUlZB4"
 
 print("Starting up your bot my Tony...")
 
-def start(update, context):
+def start_command(update, context):
     update.message.reply_text ('Hello {user} welcome to my bot') 
    
+def handle_response (text: str) -> str:
+    if hello In text
+
+    return 'hello my friend '
+
+
+dp.add.handle(commandHandler) ('start' start_command))
