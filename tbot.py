1301f8e55121511f87f890970e0508a886ef0819
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2022-12-15 22:16:14 +0530

    Update tbot.py

--- tbot.py
+++ tbot.py
@@ -4,7 +4,7 @@ Token = "5809892449:AAFE_GhVSKIcTbdya4WIg0qs9AXk5cUlZB4"
 
 print("Starting up your bot my Tony...")
 
-def start(update, context)
+def start(update, context):
     update.message.reply_text ('Hello {user} welcome to my bot'
 
 bot.run()      
