bb9a40577d67bbd25ee5b33d54bb0f71b91478bb
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2022-12-15 22:11:49 +0530

    Create tbot.py

---
+++ tbot.py
@@ -0,0 +1,10 @@
+from telegram.ext import *
+
+Token = "5809892449:AAFE_GhVSKIcTbdya4WIg0qs9AXk5cUlZB4"
+
+print("Starting up your bot my Tony...")
+
+def start(update, context)
+    update.message.reply_text ('Hello {user} welcome to my bot'
+
+bot.run()      
