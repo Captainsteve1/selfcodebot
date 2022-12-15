2bea7f5f20da167407d4a8e1a51dff28c6ed4b63
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2022-12-15 22:54:57 +0530

    Update tbot.py

--- tbot.py
+++ tbot.py
@@ -2,7 +2,7 @@ from program import client, filters
 
 bot = client(
          "My fist self code",
-      api_id = "15855531"
+      api_id = 15855531,
       api_hash = "31e0b87de4285ebff259e003f58bf469"
       bot_token = "5809892449:AAFE_GhVSKIcTbdya4WIg0qs9AXk5cUlZB4"
 )
