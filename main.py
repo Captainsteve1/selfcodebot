6672a55e8f57e1ac6270403130c1e474967f2e18
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2022-12-18 15:16:16 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from pyrogram import Client, filters
+import config 
+
+Tbot = Client("Myacc", Api_id=config.Api_id, Api_hash=config.Api_hash, Bot_Token=config.Bot_Token)
+
+print("Please wait setting up your vars")
+
+@Tbot.on.messagd(filters.command('start') & filters.private
+def welcome(client, message):
+    message.reply_text(text="Hello {user} welcome to Test bot join @tmirrorleech")
+    
+Tbot.run() # to start running bot!   
+print("Bot started.. go & chk")
