fd12b998f202a88efe7cf55f2e203202d795ca2a
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2022-12-18 10:28:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ BOT_TOKEN = "5809892449:AAFE_GhVSKIcTbdya4WIg0qs9AXk5cUlZB4"
 def start_command(update, context):
     update.message.reply_text("Hello {update.message.chat.id} welcome to my bot'!")
 
+
 def help_command(update, context):
     update.message.reply_text("Help commands {update.message.chat.id} welcome to my bot'!")
+
 
 def handle_message(update, context):
     text = str(update.message.reply).lower()
     logging.info(f'user {update.message.chat.id} says: {text}')
 
    #bot response to user
-    update.messags.reply_text{text}
+    response = responses.get_response(text)
+    update.message.reply_text(response)
 
 
 if __name__ == '__main__':
