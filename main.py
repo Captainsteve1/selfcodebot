0ca86ea39281ef6c4b386b30a45e5ca988e77084
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2022-12-18 15:36:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print("Please wait setting up your vars to start your bot")
 
 @Tbot.on_message(filters.command('start') & filters.private)
 def welcome(client, message):
-    message.reply_text(text="Hello {message.from.user.mention} welcome to Test bot join @tmirrorleech")
+    message.reply_text(text="Hello! welcome to Test bot join @tmirrorleech")
 
 print("Owner found @tony9848")
 print("Bot started.. go & check your bot")
