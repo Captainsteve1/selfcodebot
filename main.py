5cd097172186e61a98ac60a0365960ef0a6054c8
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2022-12-18 16:32:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from pyrogram import Client, filters
 import config 
 import os
 import sys
+from telegram.ext import Filters, MessageHandler
 import asyncio
 from os import path as os_path
+from subprocess import run as srun
 
 Tbot = Client(name="Myacc", api_id=config.Api_id, api_hash=config.Api_hash, bot_token=config.Bot_Token)
 
@@ -17,36 +19,29 @@ print("Owner found @tony9848")
 print("Bot started.. go & check your bot")
 
 @Tbots.on.message(filters.command('shell') & filters.private)
-def tg_s_Handler(bot: Tbot, message: Message):
-    cmd = message.text.split(' ', 1)
-    sts = await message.reply_text("Please wait ....")
-    if len(cmd) == 1:
-        return await sts.edit('**Send a command to execute**')
-    cmd = cmd[1]
-    for check in cmd.split(" "):
-        if check.upper().endswith(BLACKLISTED_EXTENSIONS):
-            return await sts.edit("you can't execute this cmd")
+def shell(update, context):
+    message = update.effective_message
+    cmd = message.text
+    process = srun(cmd, capture_output=True, shell=True)
     reply = ''
-    stderr, stdout = await run_comman_d(cmd)
-    newstdout = ""
-    for line in stdout.split("\n"):
-        if not line.upper().endswith(BLACKLISTED_EXTENSIONS):
-            newstdout += line + "\n"
-    if len(newstdout) != 0:
-        reply += f"<b>Stdout</b>\n<code>{newstdout}</code>\n"
+    stdout = process.stdout.decode('utf-8')
+    stderr = process.stderr.decode('utf-8')
+    if len(stdout) != 0:
+        reply += f"<b>Stdout</b>\n<code>{stdout}</code>\n"
     if len(stderr) != 0:
         reply += f"<b>Stderr</b>\n<code>{stderr}</code>\n"
     if len(reply) > 3000:
         with open('output.txt', 'w') as file:
             file.write(reply)
         with open('output.txt', 'rb') as doc:
-            await message.reply_document(
+            context.bot.send_document(
                 document=doc,
-                caption=f"`{cmd}`")
-            await sts.delete()
+                filename=doc.name,
+                reply_to_message_id=message.message_id,
+                chat_id=message.chat_id)
     elif len(reply) != 0:
-        await sts.edit(reply)
+        sendMessage(reply, context.bot, update.message)
     else:
-        await sts.edit('Executed')
+        sendMessage('Executed', context.bot, update.message)
 
-Tbot.run() # to start running bot!  
+Tbot.run()
